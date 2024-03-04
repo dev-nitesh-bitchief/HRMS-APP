1a1446385b127da9dc700cb8abca39581bc4d189
VanshMakhija21 <makhijaji100@gmail.com>
2024-03-04 12:33:26 +0530

    emploee file added

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var eworkInfo = require('./routes/emp-work-info');
-var employee = require('./routes/employee');
+var employee = require('./routes/Employee');
 
 var app = express();
 
