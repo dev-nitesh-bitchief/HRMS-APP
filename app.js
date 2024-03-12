091a688be667009997b05707842416c26bc10b7c
VanshMakhija21 <makhijaji100@gmail.com>
2024-03-12 09:22:36 +0530

    new file upload

--- app.js
+++ app.js
@@ -8,25 +8,23 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+
 var salaryRouter = require('./routes/salary');
 var salarytypeRouter = require('./routes/salarytype');
 
 
-var role=require('./routes/Role');
-var docs= require('./routes/Document');
+var role = require('./routes/Role');
+var docs = require('./routes/Document');
 var eworkInfo = require('./routes/emp-work-info');
 var employee = require('./routes/Employee');
 
 var holiday = require('./routes/public_holiday')
 
-var feedback =require('./routes/Feedback');
+var feedback = require('./routes/Feedback');
 var permission = require('./routes/Permission');
-var subscription= require('./routes/Subscription');
+var subscription = require('./routes/Subscription');
 
-var holiday = require('./routes/public-holiday');
 
-var expense_category = require('./routes/expense_category');
-var leave = require('./routes/leave');
 
 var Leave_request = require('./routes/Leave_request');
 var Leave_policy = require('./routes/Leave_policy');
@@ -36,12 +34,15 @@ var Leave_allocation = require('./routes/Leave_allocation');
 
 
 
+var Expense_category = require('./routes/Expense_category');
+var Payment_method = require('./routes/Payment_method');
+var Expense = require('./routes/Expense');
 
-
-
-
+var Email_template = require('./routes/Email_temp');
+var Smtp = require('./routes/Smtp');
 
 var app = express();
+app.use(express.static('styling'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -54,50 +55,48 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+
 app.use('/users', usersRouter);
 
 app.use('/salary', salaryRouter);
 app.use('/salarytype', salarytypeRouter);
 
+app.use('/role', role);
 
-app.use('/role',role);
-
-app.use('/ework',eworkInfo);
-app.use('/employee',employee);
-app.use('/permission',permission);
-app.use('/subscription',subscription);
-app.use('/docs',docs);
-app.use('/feedback',feedback);
+app.use('/ework', eworkInfo);
+app.use('/employee', employee);
+app.use('/permission', permission);
+app.use('/subscription', subscription);
+app.use('/docs', docs);
+app.use('/feedback', feedback);
 
 
 // app.use('/leave', leave);
 app.use('/Leave', Leave_request);
 app.use('/Leave-policy', Leave_policy);
 app.use('/Leave-balance', Leave_balance);
-app.use('/Leave-type' , Leave_type);
-app.use('/Leave-allocation' , Leave_allocation);
+app.use('/Leave-type', Leave_type);
+app.use('/Leave-allocation', Leave_allocation);
 
 
+app.use('/holiday', holiday);
 
 
-app.use('/leave', leave);
-app.use('/holiday',holiday);
+app.use('/Expense_category', Expense_category);
+app.use('/Payment_method', Payment_method);
+app.use('/Expense', Expense);
 
 
+app.use('/Email_template', Email_template);
+app.use('/Smtp', Smtp);
 
-app.use('/expense',expense_category);
-
-
-
-
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
+//catch 404 and forward to error handler
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,204 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const secretKey = "$1234";
+const bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
 
+var db = require('../connection/db');
+// Import the LocalStorage class from the node-localstorage package
+const { LocalStorage } = require('node-localstorage');
 
+// Create a new instance of LocalStorage
+const localStorage = new LocalStorage('./scratch');
+var db = require('../connection/db');
 
-<<<<<<< HEAD
-
-var db= require('../connection/db');
-
-
-=======
-var db= require('../connection/db');
-
-
-
-
-
->>>>>>> 18860c2af9d04f9e6a692b0275f7a4489814fbf4
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('login', { layout: 'empty_layout' });
+});
+
+// router.get('/login', function(req, res, next) {
+//   res.render('login', { title: 'Express' });
+// });
+
+// router.get('/login', function (req, res) {
+//   res.render('home')
+// });
+
+router.get('/main', function (req, res) {
+  res.render('main');
+});
+
+router.get('/dashboard', function (req, res) {
+  res.render('dashboard');
+});
+
+router.get('/dynamic-options-payment-method', (req, res) => {
+  const sql = 'select * from Payment_method';
+  db.query(sql, (error, results) => {
+    if (error) {
+      console.error('Error retrieving data from database: ' + error.stack);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+router.get('/dynamic-options-expense-category', (req, res) => {
+  const sql = 'select * from Expense_category';
+  db.query(sql, (error, results) => {
+    if (error) {
+      console.error('Error retrieving data from database: ' + error.stack);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json(results);
+  });
+});
+
+router.get('/dynamic-employee-field', (req, res) => {
+  const sql = `SELECT id,CONCAT(firstName, ' ', lastName) AS employeeName
+  FROM Employee`;
+  db.query(sql, (error, results) => {
+    if (error) {
+      console.error('Error retrieving data from database: ' + error.stack);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    console.log("result inside employee dynamic ", results);
+    res.json(results);
+  });
+});
+
+router.get('/dynamic-states', (req, res) => {
+  console.log('Inside  state route ');
+  const sqlQuery = `SELECT * FROM State`;
+  db.query(sqlQuery, (error, results) => {
+    if (error) {
+      console.error('Error retrieving data from database: ' + error.stack);
+      res.status(500).json({ error: 'Internal server error' });
+      return;
+    }
+    console.log(results);
+    res.json(results);
+  });
 });
 
 
 
+
+const authenticateUser = (req, res, next) => {
+  if (req.session.user_visit) {
+    // User is already authenticated
+    return next();
+  }
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized 123' });
+  }
+  try {
+    const decoded = jwt.verify(token, secretKey);
+    req.user = decoded.username;
+    // req.email=decoded.email;
+    req.role_id = decoded.role_id;
+    return next();
+  } catch (error) {
+    res.status(401).json({ message: 'Unauthorized 456' });
+  }
+
+};
+
+var name;
+
+router.post('/login', (req, res) => {
+  // Get the id and password from the request body
+  // const id = req.body.user_name;
+  // const password = req.body.password;
+  const { user_name, password } = req.body;
+  console.log(user_name);
+  console.log(password);
+
+  // module.exports.user_name = user_name;
+  // global.userNAME = user_name;
+
+  // module.exports = { user_name };
+  name = user_name;
+  console.log('name is ', name);
+  // module.exports =  { name }  ;
+
+  localStorage.setItem('username', name);
+
+  // Validate the input
+  if (!user_name || !password) {
+    res.send('Please enter your id and password');
+    return;
+  }
+  // Query the database to get the hash by the id
+  db.query('SELECT * FROM User WHERE username = ?', [user_name], (err, result, fields) => {
+    if (err) {
+      res.send('Database error');
+      return;
+    }
+    // Check if the query returned any row
+    if (result.length > 0) {
+      // Get the hash from the results
+      const user = result[0];
+      const hash = user.password;
+      var userName = user.username;
+      var role_id = user.Role_id;
+
+      // Compare the password with the hash
+      bcrypt.compare(password, hash, (err, result) => {
+        if (err) {
+          res.send('Hashing error');
+          return;
+        }
+
+        // Result is a boolean indicating if the password and hash match
+        if (result) {
+          // The password is correct
+
+          // Set the session to indicate the user is logged in
+          // req.session.loggedin = true;
+          // req.session.visit = 1;
+
+
+          //  var email= user.email;
+
+          const payload = { userName, role_id };
+          console.log(payload);
+          console.log(result);
+          global.token = jwt.sign(payload, secretKey, { expiresIn: '1h' });
+
+          // Set the exported user_name value
+
+          // module.exports.userName = userName;
+
+          res.redirect('/main');
+
+        } else {
+          // The password is wrong
+          res.send('Invalid id or password');
+        }
+      });
+    } else {
+      // The id does not exist in the database
+      res.send('Id not found');
+    }
+  });
+});
+function destroyToken() {
+  global.token = null; // Clear the token
+}
+
+router.get('/logout', (req, res) => {
+  destroyToken();
+  console.log('value of name is ', name);
+  name = null;
+  console.log('value of name is ', name);
+  res.redirect('/');
+});
 module.exports = router;
